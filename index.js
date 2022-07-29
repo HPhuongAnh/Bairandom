cf743a94dcbf8e445132d75e591e1f38a9b1ce06
Phuong <hpa19112002@gmail.com>
2022-07-29 21:03:00 +0700

    bairandom

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const names = ['Anh', 'Trung', 'Thạnh'];
+
 document.getElementById("random").onclick = () => {
-    const valueRandom = Math.random();
-    const isCheckAttendance = !!Math.round(valueRandom);
-    document.getElementsByTagName("h2")[0].innerText = isCheckAttendance
-        ? "Có"
-        : "Không";
+    const valueRandom = Math.round(Math.random() * 2, 1);
+    console.log(valueRandom);
+    document.getElementsByTagName("h2")[0].innerText = names[valueRandom];
 };
 
